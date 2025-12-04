a866918a43808961a8e5c31812231b32a88530d6
imarikacos <imarikacos@gmail.com>
2025-12-04 09:33:11 +0300

    Create server_worker.js

---
+++ server_worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'luxearn-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/lexearn.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(function(error) {
+        console.log('Cache failed:', error);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
